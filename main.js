ddcaf391dce80b147783dd18d664d25e747d1a2f
Gints <gintskristaps13@gmail.com>
2024-10-22 11:19:07 +0300

    table direction clarification

--- main.js
+++ main.js
@@ -69,6 +69,11 @@ function subBytes(state) {
 function shiftRows(state) {
     const temp = Array.from(state);
 
+    // 0  5  8 12
+    // 1  5  9 13
+    // 2  6 10 14
+    // 3  7 11 15
+
     temp[1] = state[5];
     temp[5] = state[9];
     temp[9] = state[13];
